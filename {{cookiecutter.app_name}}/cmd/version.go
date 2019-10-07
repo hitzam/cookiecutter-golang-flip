a8a974448687dc5fe492d57583a70c221a89d154
zackijack <m.zackky@gmail.com>
2019-10-07 22:25:35 +0700

    Fix indent

--- {{cookiecutter.app_name}}/cmd/version.go
+++ {{cookiecutter.app_name}}/cmd/version.go
@@ -13,12 +13,12 @@ var versionCmd = &cobra.Command{
         Short: "Print the version number of {{ cookiecutter.app_name }}",
         Long:  `All software has versions. This is {{ cookiecutter.app_name }}`,
         Run: func(cmd *cobra.Command, args []string) {
-                fmt.Println("Build Date:", version.BuildDate)
-                fmt.Println("Git Commit:", version.GitCommit)
-                fmt.Println("Version:", version.Version)
-                fmt.Println("Environment:", version.Environment)
-                fmt.Println("Go Version:", version.GoVersion)
-                fmt.Println("OS / Arch:", version.OsArch)
+			fmt.Println("Build Date:", version.BuildDate)
+			fmt.Println("Git Commit:", version.GitCommit)
+			fmt.Println("Version:", version.Version)
+			fmt.Println("Environment:", version.Environment)
+			fmt.Println("Go Version:", version.GoVersion)
+			fmt.Println("OS / Arch:", version.OsArch)
         },
 }
 
